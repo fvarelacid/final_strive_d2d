c1bff63dc478a0cb56eeeac4bf9cd29e10d903e2
Francisco <fvarelacid@gmail.com>
2021-09-21 12:37:09 +0100

    Get_sentences and clean_sentences fn

--- 7. Natural Language Processing/2. Embedding/data_handler.py
+++ 7. Natural Language Processing/2. Embedding/data_handler.py
@@ -8,3 +8,30 @@ def preprocess(pth):
 
             with open("preprocessed.txt", "w") as p:
                 p.writelines(lines)
+
+def get_sentences(pth):
+
+    f = open(pth, 'r')
+
+    lines = f.readlines()
+
+    sentences = [line.split() for line in lines]
+
+    f.close()
+
+    return sentences
+
+
+def clean_sentences(sentences):
+
+    i = 0
+
+    while i < len(sentences):
+
+        if sentences[i] == []:
+            sentences.pop(i)
+        
+        else:
+            i += 1
+
+    return sentences
