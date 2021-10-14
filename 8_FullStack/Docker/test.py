a55e53ef7afb5b0fa09f11e05e5c780d1169313d
Francisco <fvarelacid@gmail.com>
2021-10-14 17:09:07 +0100

    Docker & Docker API INIT

---
+++ 8_FullStack/Docker/test.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, "html.parser")
+
+movies = soup.find_all('td', class_="titleColumn")
+
+for movie in movies:
+    print(movie.find('a').text)
+
